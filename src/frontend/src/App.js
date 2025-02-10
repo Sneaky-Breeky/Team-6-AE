ad1f4e3553f4f27878a49344ef3b8c9ab699acbc
Mandy Deng <miaodan@students.cs.ubc.ca>
2025-02-10 06:34:08 -0800

    general activity log page made with customized date to string, side menu to be added

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,12 +3,14 @@ import Login from './pages/Login';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import UserDashboard from './pages/user/UserDashboard';
 import { isAdmin, isLoggedIn } from './utils/auth';
+import ActivityLog from './pages/ActivityLog';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/ActivityLog" element={<ActivityLog />} />
 
         {isLoggedIn() && isAdmin() && (
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
