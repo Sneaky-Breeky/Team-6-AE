792271c0a2c0d06c4bb5bf93d023990e155cff45
Mandy Deng <miaodan@students.cs.ubc.ca>
2025-02-11 04:34:43 -0800

    rebase

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,18 +3,13 @@ import Login from './pages/Login';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import UserDashboard from './pages/user/UserDashboard';
 import { isAdmin, isLoggedIn } from './utils/auth';
-<<<<<<< HEAD
 import SideMenu from './components/SideMenu'; 
 import AppNavbar from './components/AppNavbar'; 
 import Box from '@mui/material/Box';
-=======
-import ActivityLog from './pages/ActivityLog';
->>>>>>> 296c5a0 (general activity log page made with customized date to string, side menu to be added)
 
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
       {isLoggedIn() ? (
         <Box sx={{ display: 'flex' }}>
           <SideMenu />
@@ -37,30 +32,6 @@ function App() {
           <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       )}
-=======
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/ActivityLog" element={<ActivityLog />} />
-
-        {isLoggedIn() && isAdmin() && (
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        )}
-        {isLoggedIn() && !isAdmin() && (
-          <Route path="/user/dashboard" element={<UserDashboard />} />
-        )}
-
-        <Route
-          path="/"
-          element={
-            isLoggedIn()
-              ? <Navigate to={isAdmin() ? '/admin/dashboard' : '/user/dashboard'} />
-              : <Navigate to="/login" />
-          }
-        />
-
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
->>>>>>> 296c5a0 (general activity log page made with customized date to string, side menu to be added)
     </Router>
   );
 }
