1db5a32fa36fa2a65d899eb4130fecf12f3632f3
gary <guanyunze4251@gmail.com>
2025-03-11 00:53:54 +0900

    backend is back

---
+++ src/Team-6-AE-DAM-Backend/src/main/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DAMBackend.Models;
+using Microsoft.EntityFrameworkCore;
+using File = DAMBackend.Models.File;
+
+namespace DAMBackend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<FileModel> Files { get; set; }
+        public DbSet<ProjectModel> Projects { get; set; }
+
+        public DbSet<TagModel> Tags { get; set; }
+    }
+}
